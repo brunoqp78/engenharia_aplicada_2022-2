25c2c85188783ef486dbf40f834d130e0155f19e
bruno <bruno@DESKTOP-S4N53FP>
2022-09-09 19:19:45 -0300

    Etapa 1 - Criação do projeto

---
+++ projeto/src/main/java/org/iftm/projeto/app/Programa.java
@@ -0,0 +1,14 @@
+package org.iftm.projeto.app;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Programa {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto");
+		EntityManager em = emf.createEntityManager();
+		em.close();
+		emf.close();
+	}
+}
